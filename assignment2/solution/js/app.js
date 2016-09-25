92cfdaa7591aa562858007393bff6e3f09eaf6a1
jpaulofb <jpfb@icomp.ufam.edu.br>
2016-09-25 13:11:51 -0400

    assignment 2 controllers stubs

--- assignment2/solution/js/app.js
+++ assignment2/solution/js/app.js
@@ -1,4 +1,23 @@
 (function(){
+'use strict';
 
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('checkOffService', ShoppingListCheckOffService);
+
+// TODO
+ToBuyShoppingController.$inject = ['$scope', 'checkOffService'];
+function ToBuyShoppingController($scope, checkOffService) {
+    var toBuyCtrlr = this;
+
+}
+
+// TODO
+AlreadyBoughtShoppingController.$inject = ['$scope', 'checkOffService'];
+function AlreadyBoughtShoppingController($scope, checkOffService) {
+    var boughtCtrlr = this
+
+}
 
 })();
