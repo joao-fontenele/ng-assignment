fba04490f74e5744c76acd390a1e67e8dbea15d0
jpaulofb <jpfb@icomp.ufam.edu.br>
2016-09-25 13:17:12 -0400

    assignment 2 adding check off custom service

--- assignment2/solution/js/app.js
+++ assignment2/solution/js/app.js
@@ -20,4 +20,58 @@ function AlreadyBoughtShoppingController($scope, checkOffService) {
 
 }
 
+function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuy = [
+        {
+            name: 'potato chips',
+            quantity: 'a dozen bags',
+        },
+        {
+            name: 'cheese',
+            quantity: '100',
+        },
+        {
+            name: 'ham',
+            quantity: '100',
+        },
+        {
+            name: 'bread',
+            quantity: '100',
+        },
+        {
+            name: 'cola',
+            quantity: '10',
+        },
+    ];
+    var bought = [];
+
+    service.buy = function(index) {
+        if (index === undefined || toBuy[index] === undefined) {
+            throw new Error('Invalid index to buy(' + index + ')');
+        } else {
+            var item = toBuy.splice(index, 1);
+            bought.push(item);
+        }
+    };
+
+    service.addToBuy = function(item) {
+        if (!item.name || !item.quantity) {
+            throw new Error('Invalid item, need name and quantity properties.',
+                  item);
+        } else {
+            toBuy.push(item);
+        }
+    }
+
+    service.getToBuyList() = function() {
+        return toBuy;
+    };
+
+    service.getBoughtList() = function() {
+        return bought;
+    };
+}
+
 })();
